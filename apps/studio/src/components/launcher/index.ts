f983851c6a70f26d386e16217f3405906eab1f3e
Abian Suarez <abiansuarezbrito@gmail.com>
2026-01-28 18:41:03 +0000

    chore: delete studio

--- apps/studio/src/components/launcher/index.ts
+++
@@ -1,5 +0,0 @@
-export { Footer } from "./footer";
-export { type Project, ProjectCard } from "./project-card";
-export { ProjectList } from "./project-list";
-export { SearchInput } from "./search-input";
-export { type ProjectStatus, StatusBadge } from "./status-badge";
